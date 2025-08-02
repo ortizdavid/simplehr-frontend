c20f265bb5687f2eb4a1354e79dd5c80563a6fee
ortizdavid <ortizaad1994@gmail.com>
2025-08-02 22:11:47 +0100

    created: initail folder structure

---
+++ create-module.sh
@@ -0,0 +1,7 @@
+ng g m modules/employees --route=employees --standalone --flat
+ng g m modules/reports --route=reports --standalone --flat
+ng g m modules/identity --route=identity --standalone --flat
+ng g m modules/payroll --route=payroll --standalone --flat
+ng g m modules/references --route=references --standalone --flat
+ng g m modules/configurations --route=configurations --standalone --flat
+
